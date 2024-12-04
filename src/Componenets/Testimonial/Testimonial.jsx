6827cc60d7577355fb0ce57a01a353bbbd5dbf19
Mohsin <167190542+mohsin-develope-code@users.noreply.github.com>
2024-12-04 23:40:41 +0530

    Update Testimonial.jsx

--- src/Componenets/Testimonial/Testimonial.jsx
+++ src/Componenets/Testimonial/Testimonial.jsx
@@ -1,39 +1,46 @@
+import user1 from "../../assets/profile/user1.jpg"
+import user2 from "../../assets/profile/user2.jpg"
+import user3 from "../../assets/profile/user3.jpg"
+import user4 from "../../assets/profile/user4.jpg"
+import user5 from "../../assets/profile/user5.jpg"
+import user6 from "../../assets/profile/user6.jpg"
+
 
 const testimonials = [
     {
       user: "John Doe",
       company: "Stellar Solutions",
-      image: "src/assets/profile/user1.jpg",
+      image: user1,
       text: "I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.",
     },
     {
       user: "Jane Smith",
       company: "Blue Horizon Technologies",
-      image: "src/assets/profile/user2.jpg",
+      image: user2,
       text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
     },
     {
       user: "David Johnson",
       company: "Quantum Innovations",
-      image: "src/assets/profile/user3.jpg",
+      image: user3,
       text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
     },
     {
       user: "Ronee Brown",
       company: "Fusion Dynamics",
-      image: "src/assets/profile/user4.jpg",
+      image: user4,
       text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
     },
     {
       user: "Michael Wilson",
       company: "Visionary Creations",
-      image: "src/assets/profile/user5.jpg",
+      image: user5,
       text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
     },
     {
       user: "Emily Davis",
       company: "Synergy Systems",
-      image: "src/assets/profile/user6.jpg",
+      image: user6,
       text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
     },
   ];
